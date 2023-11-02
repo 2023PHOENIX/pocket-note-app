e4b042640ea07aa8456d664f87f423c36a567a19
abhishek <abhishekshrivastav1920@gmail.com>
2023-11-02 12:37:24 +0530

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "./index.css";
-import FormProvider from "./context/formProvider.jsx";
+import FormProvider from "./context/FormProvider.jsx";
 import ChatGroupProvider from "./context/ChatGroupProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
